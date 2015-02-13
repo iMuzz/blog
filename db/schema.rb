d3d527686d5c172dea7359d79a91ace549fff87c
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-02-12 23:17:25 -0800

    create schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150213055839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.string   "author"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "current_readings", force: :cascade do |t|
+    t.integer  "book_id"
+    t.integer  "progress"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reads", force: :cascade do |t|
+    t.integer  "book_id"
+    t.integer  "stars"
+    t.string   "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
