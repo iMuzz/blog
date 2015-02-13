ea21ad29a6339fde55bc4da90043fa3395dc9c69
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-02-12 22:57:50 -0800

    create update method that pulls currently reading from goodreads api

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,8 @@
 module WelcomeHelper
+  def update
+    current_reading = GoodReadsClient.shelf("17272747", "currently-reading")
+    current_reading[:books].each do |book|
+      Book.create(title: book[:book][:title], author: book[:book][:authors][:author][:name], image: book[:book][:image_url])
+    end
+  end
 end
