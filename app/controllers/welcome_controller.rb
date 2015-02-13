96057565576f01ad77bfef4cf17b2838b2596b0d
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-02-12 23:19:37 -0800

    iterate over books to create card

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   include WelcomeHelper
   
   def home
-    # @currently_reading = Book.all
+    @currently_reading = Book.all
     render "books"
   end
 end
