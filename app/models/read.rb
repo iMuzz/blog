19d8aaa6ff2427d91e87645b0263c62c8dbe9fde
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-02-13 22:13:39 -0800

    remove from currently reading shelf

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,4 +1,13 @@
 class Read < ActiveRecord::Base
   validates_uniqueness_of :book_id
   validates_presence_of :book_id, :stars
+
+  after_create :removeFromCurrentlyReading
+
+  private
+
+  def removeFromCurrentlyReading
+    book = CurrentReading.find_by(id: self.book_id)
+    book.destroy if not book
+  end
 end
