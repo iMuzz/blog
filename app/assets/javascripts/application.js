2378e388b0aa289e3e11084d97acc70ce296a4d9
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-02-14 00:09:40 -0800

    semicolons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,18 +17,18 @@
 $( document ).ready(function() {
 	shakeButton()
 	openNav()
-});
+})
 
 function shakeButton(){
 	setInterval(function(){
 	      $('#nav-button').addClass("animated swing").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd onanimationend', function() {
 			$(this).removeClass('animated swing')
 		  })
-    },4000);
+    },4000)
 }
 
 function openNav(){
 	$('#nav-button').click(function() {
 		$('.sidebar').sidebar('toggle')
-	});
+	})
 }
