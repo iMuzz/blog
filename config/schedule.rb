1f9df91f1a9b3ec01043a52d31681e66ed2bd1e9
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-03-04 17:00:19 -0800

    add cron job for production

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
-every :day, :at => '2:45pm' do
-  rake 'fetch_books:goodreads', :environment => 'development', :output => 'log/cron.log'
+every 1.minute do 
+  rake "fetch_books:goodreads", :environment => 'development', :output => 'log/cron.log'
 end
 
 every 1.minute do 
-  rake "fetch_books:goodreads", :environment => 'development', :output => 'log/cron.log'
+  rake "fetch_books:goodreads", :environment => 'production', :output => 'log/cron.log'
 end
